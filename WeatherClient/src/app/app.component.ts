498ad31455910cdc3bfedaa7fd8dbdf88a63fe0b
Eduardo Salazar <eduardosalaz@outlook.com>
2022-05-04 14:10:40 -0500

    client html
    kinda ugly

--- WeatherClient/src/app/app.component.ts
+++ WeatherClient/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import {latLng, tileLayer} from "leaflet";
+import {LatLng, latLng, LeafletMouseEvent, tileLayer} from "leaflet";
+import {HttpClient} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +11,75 @@ import {latLng, tileLayer} from "leaflet";
 
 })
 export class AppComponent {
+
+  constructor(private httpClient: HttpClient) { }
+
   title = 'WeatherClient';
   options = {
     layers: [
-      tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 18, attribution: '...'})
     ],
-    zoom: 5,
-    center: latLng(46.879966, -121.726909)
+    center: latLng(46.879966, -121.726909),
+    zoom: 3
   };
+  zoom: any;
+  center: any;
+  lat: number | undefined;
+  long: number | undefined;
+  metric = true;
+  temp = undefined;
+  presion = undefined;
+  humedad = undefined;
+  pais = undefined;
+  units: string | undefined;
+  ciudad = undefined;
+
+  handleEvent(click: string, $event: LeafletMouseEvent) {
+    this.lat = $event.latlng.lat;
+    this.long = $event.latlng.lng;
+  }
+
+  onZoomChange($event: number) {
+
+  }
+
+  onCenterChange($event: LatLng) {
+    //console.log($event)
+  }
+
+  // @ts-ignore
+  submitCoords() {
+    if (this.lat === undefined) {
+      alert("Selecciona un lugar en el mapa");
+    } else {
+      let apiUrl = "http://localhost:5092";
+      let requestUrl = `/weather_data/${this.lat}/${this.long}/${this.metric}`
+      if (!this.metric){
+        this.units = "Fahrenheit";
+      }else{
+        this.units = "Celsius";
+      }
+      this.httpClient.get(apiUrl+requestUrl).subscribe(data =>{
+        // @ts-ignore
+        this.temp = data.main.temp;
+        // @ts-ignore
+        this.presion = data.main.pressure;
+        // @ts-ignore
+        this.humedad = data.main.humidity;
+        // @ts-ignore
+        this.pais = data.sys.country;
+        // @ts-ignore
+        this.ciudad = data.name;
+      })
+    }
+  }
+
+  onChange(value: any) {
+    if (value === "Fahrenheit") {
+      this.metric = false;
+
+    } else {
+      this.metric = true;
+    }
+  }
 }
--- WeatherClient/src/app/app.module.ts
+++ WeatherClient/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { LeafletModule } from "@asymmetrik/ngx-leaflet";
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './components/map/map.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { MapComponent } from './components/map/map.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    LeafletModule
+    LeafletModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
